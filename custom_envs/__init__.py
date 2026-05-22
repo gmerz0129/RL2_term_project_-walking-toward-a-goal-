5e5673e8a77a8e3baa7128dac11cefe41e4a0cd2
Kim minsik <mandoo0129@catholic.ac.kr>
2026-05-22 17:15:29 +0900

    Update __init__.py

--- custom_envs/__init__.py
+++ custom_envs/__init__.py
@@ -5,7 +5,7 @@ from gymnasium.envs.registration import register
 from .humanoid_hole_env import HumanoidHoleEnv
 
 
-# Gymnasium registry에 새 환경 등록
+# Gymnasium registry에 새로운 환경을 등록
 register(
     id="HumanoidHole-v0",
     entry_point="custom_envs.humanoid_hole_env:HumanoidHoleEnv",
